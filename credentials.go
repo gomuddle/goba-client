f96687f4a6153f04c8bf1865bbbf1de2883871fd
Daspoet <cederk2306@gmail.com>
2021-09-24 23:47:40 +0200

    feat: add credentials

---
+++ credentials.go
@@ -0,0 +1,10 @@
+package goba_client
+
+// Credentials represent the client's credentials.
+type Credentials struct {
+	// Username is the Credentials' password.
+	Username string `json:"username,omitempty"`
+
+	// Password is the Credentials' password.
+	Password string `json:"password,omitempty"`
+}
